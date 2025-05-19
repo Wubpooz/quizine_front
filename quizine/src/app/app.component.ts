590d4d23ff67ae4ee7e2f3505082de86877d2eef
Wubpooz <mathieu.waharte@gmail.com>
2025-05-19 17:47:35 +0200

    draft of user invite component

--- quizine/src/app/app.component.ts
+++ quizine/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserInviteComponent } from "./components/user-invite/user-invite.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserInviteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ quizine/src/app/components/user-invite/user-invite.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { User } from '../../models/userModel';
+import {FriendsStore} from '../../stores/friends.store';
+
+
+@Component({
+  selector: 'user-invite',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './user-invite.component.html',
+  styleUrl: './user-invite.component.css'
+})
+export class UserInviteComponent {
+  // ViewChild
+  inviteForm: FormGroup;
+
+  friends: User[] = [];
+  users: User[];
+
+  friendListSubscription: any;
+
+
+  constructor(private fb: FormBuilder,
+      private friendsStore: FriendsStore
+      ) {
+    this.inviteForm = this.fb.group({
+      // email: ['', [Validators.required, Validators.email]]
+    });
+
+    this.users = [];
+
+    this.friendsStore.friendList$.subscribe((friends: User[]) => {
+      this.friends = friends;
+    });
+  }
+
+  onSearch(event: any) {
+    const searchTerm = event.target.value.toLowerCase();
+    this.friends = this.friends.filter((friend: User) => {
+      return friend.name.toLowerCase().includes(searchTerm) || friend.email.toLowerCase().includes(searchTerm);
+    });
+  }
+
+  onCheckboxChange(event: any) {
+    const checkboxes = event.target.parentElement.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox: HTMLInputElement) => {
+      if (checkbox !== event.target) {
+        checkbox.checked = false;
+      }
+    });
+  }
+
+  onSubmit() {
+    if (this.inviteForm.valid) {
+      // Handle form submission
+      console.log(this.inviteForm.value);
+    }
+  }
+
+  onSkip() {
+    // Handle skip action
+    console.log('Skip action triggered');
+  }
+
+  onClose() {
+    // Handle close action
+    console.log('Close action triggered');
+  }
+}
