c1c750b09af081c88c540d1d801058c831af73bb
Wubpooz <mathieu.waharte@gmail.com>
2025-05-20 12:46:46 +0200

    cleaner and hover states

--- quizine/src/app/components/user-invite/user-invite.component.ts
+++ quizine/src/app/components/user-invite/user-invite.component.ts
@@ -4,7 +4,6 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 import { User } from '../../models/userModel';
 import {FriendsStore} from '../../stores/friends.store';
 
-
 @Component({
   selector: 'user-invite',
   standalone: true,
