1eb4b7e9d3c6eef9e97a88f6246d44868b932ea8
Wubpooz <mathieu.waharte@gmail.com>
2025-05-22 00:31:02 +0200

    routes

--- quizine/src/app/app.routes.ts
+++ quizine/src/app/app.routes.ts
@@ -5,12 +5,30 @@ import { LibraryComponent } from './components/library/library.component';
 import { EditQuizComponent } from './components/edit-quiz/edit-quiz.component';
 import { WaitingPageComponent } from './components/waiting-page/waiting-page.component';
 import { UserInviteComponent } from './components/user-invite/user-invite.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { QuizRecapComponent } from './components/quiz-recap/quiz-recap.component';
+import { QuizScoreComponent } from './components/quiz-score/quiz-score.component';
 
 export const routes: Routes = [
+    {path: "", redirectTo: "landing", pathMatch: "full"},
+    {path: "landing", component: LandingComponent},
+    {path: "login", component: LoginComponent},
+
+    {path: "home", component: HomePageComponent},
     {path: "explore", component: ExploreComponent},
-    {path: "quiz-preview/:id", component: QuizDescriptionComponent},
     {path: "library", component: LibraryComponent},
+    {path: "profile", component: ProfileComponent},
+
+    {path: "quiz-preview/:id", component: QuizDescriptionComponent},
     {path: "edit-quiz/:id", component: EditQuizComponent},
+
+    {path: "quiz-recap", component: QuizRecapComponent},
+    {path: "quiz-score", component: QuizScoreComponent},
+
     { path: 'waiting', component: WaitingPageComponent },
+
     { path: 'invite', component: UserInviteComponent }
 ];
