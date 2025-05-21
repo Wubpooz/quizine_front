ce40b0cd8f0c06b17151583c5be0d2794498c7ce
Mariem_Cheikh-Rouhou <meriamcheikhrouhou@gmail.com>
2025-05-21 17:05:35 +0200

    homepage

--- quizine/src/app/app.component.ts
+++ quizine/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserInviteComponent } from "./components/user-invite/user-invite.component";
+import { LoginComponent } from "./components/login/login.component";
+import { LandingComponent } from "./components/landing/landing.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserInviteComponent],
+  imports: [RouterOutlet, UserInviteComponent,LoginComponent,LandingComponent,HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ quizine/src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import {trigger,state,style, animate, transition} from '@angular/animations'
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'home-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css',
+  animations:[
+    trigger('slideInOut',[
+      state('in',style({transform: 'translateX(0)'})),
+      state('out',style({transform: 'translateX(-100)'})),
+      transition('in=>out', animate('300ms ease-in')),
+      transition('out=>in', animate('300ms ease-out'))
+
+    ])
+  ]
+})
+export class HomePageComponent {
+  isSideBarOpen=true;
+  quizzes = [
+    { title: 'Quiz d\'histoire', questions: 16, author: 'johndoe', date: '23.23.3/2025' },
+    // Add more quizzes...
+  ];
+
+  get sideBarState(){
+    return this.isSideBarOpen ? 'in' : 'out';
+  }
+
+  toggleSideBar() {
+    this.isSideBarOpen=!this.isSideBarOpen;
+  }
+  onCreateQuiz() {
+    console.log('Create Quiz button clicked');
+    // Navigate to quiz creation page
+  }
+  
+}
