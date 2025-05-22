b0bbc5f7ed8ecdfe3796e4f20b67ee4df08cd6d8
Wubpooz <mathieu.waharte@gmail.com>
2025-05-22 13:01:01 +0200

    mock profile

--- quizine/src/app/components/profile/profile.component.ts
+++ quizine/src/app/components/profile/profile.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
+  showFriends = false;
+  friends = [
+    { name: 'Alice' },
+    { name: 'Bob' },
+    { name: 'Charlie' }
+    // ...add real friends here
+  ];
 }
