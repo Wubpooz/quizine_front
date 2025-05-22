433ab598d065f1589c79579316053e9a37e10307
Wubpooz <mathieu.waharte@gmail.com>
2025-05-22 13:40:29 +0200

    profile update

--- quizine/src/app/components/profile/profile.component.ts
+++ quizine/src/app/components/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AppStore } from '../../stores/app.store';
+import { User } from '../../models/userModel';
+import { Quiz } from '../../models/quizModel';
 
 @Component({
   selector: 'app-profile',
@@ -9,11 +12,26 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  user!: User;
+  friends: User[] = [];
+  history: Quiz[] = [];
   showFriends = false;
-  friends = [
-    { name: 'Alice' },
-    { name: 'Bob' },
-    { name: 'Charlie' }
-    // ...add real friends here
-  ];
+
+  constructor(private appStore: AppStore) {
+    this.appStore.currentUser.subscribe((user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+    this.appStore.friends.subscribe((friends) => {
+      if (friends) {
+        this.friends = friends;
+      }
+    });
+    this.appStore.recentHistory.subscribe((history) => {
+      if (history) {
+        this.history = history;
+      }
+    });
+  }
 }
