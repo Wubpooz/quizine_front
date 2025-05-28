1310d6285a6ad9da49f9df813956c85ce54e0458
Wubpooz <mathieu.waharte@gmail.com>
2025-05-28 11:17:16 +0000

    cleaner profile logo and better responsive design

--- quizine/tailwind.config.js
+++ quizine/tailwind.config.js
@@ -23,6 +23,13 @@ module.exports = {
         body: ['Poppins', 'sans-serif'],
       }
     },
+    screens: {
+      xs: '440px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    }
   },
   plugins: [],
 }
