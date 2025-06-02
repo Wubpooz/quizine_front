69140d3783c645ed4d3a0ab6949849601dda4d51
Wubpooz <mathieu.waharte@gmail.com>
2025-06-02 13:46:22 +0000

    fixed sidebar alignment + closes when click outside or esc

--- quizine/src/app/components/navbar/navbar.component.ts
+++ quizine/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppStore } from '../../stores/app.store';
 import { Quiz } from '../../models/quizModel';
@@ -16,6 +16,7 @@ export class NavbarComponent {
   quizList: Quiz[] = [];
   filteredQuizList: Quiz[] = [];
   searchTerm: string = '';
+  searchDropdownOpen: boolean = false;
   
   constructor(private router: Router, private appStore: AppStore, private sidebarService: SidebarService) {
       this.appStore.init()
@@ -33,6 +34,11 @@ export class NavbarComponent {
     this.filteredQuizList = this.quizList.filter(q =>
       q.nom.toLowerCase().includes(lower)
     );
+  }
+
+  @HostListener('document:mousedown', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    this.searchDropdownOpen = false;
   }
 
   toggleSideBar() {
