95caf52a9b4d95cd5924158805fe8af66feab34e
Wubpooz <mathieu.waharte@gmail.com>
2025-06-29 01:33:19 +0200

    trying to fix env

--- quizine/src/environments/environment.vercel.ts
+++ quizine/src/environments/environment.vercel.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiEndpoint: "/api", // https://quizine-backend.vercel.app/api rewriten by vercel
   mockAuth: false,
   socket: false,
-  supabaseUrl: '${SUPABASE_URL}',
-  supabaseKey: '${SUPABASE_ANON_KEY}'
+  supabaseUrl: process.env['SUPABASE_URL'] || '',
+  supabaseKey: process.env['SUPABASE_ANON_KEY'] || ''
 };
