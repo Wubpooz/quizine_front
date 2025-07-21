9ce70b59a1c409f8f8b32d9b4ad064f78eedbb7c
Wubpooz <mathieu.waharte@gmail.com>
2025-07-21 19:51:05 +0200

    using app-button

--- quizine/src/app/components/button/button.component.ts
+++ quizine/src/app/components/button/button.component.ts
@@ -10,12 +10,14 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() label: string = "";
   @Input() type: "button" | "submit" | "reset" = "button";
-  @Input() style: "primary" | "secondary" | "warn" | "tertiary" | "error" | "text" = "primary";
-  @Input() size: "small" | "medium" | "large" | "xxlarge" = "medium";
-  @Input() icon: string = "";
+  @Input() style: "primary" | "secondary" | "warn" | "tertiary" | "border" | "error" | "text" = "primary";
+  @Input() size: "small" | "medium" | "large" | "xlarge" | "xxlarge" = "medium";
+  @Input() icon: string = ""; //TODO
   @Input() class: string = "";
-  @Input() disabled: boolean = false;
-  @Input() loading: boolean = false;
-  @Input() fullWidth: boolean = false;
+  @Input() disabled: boolean = false; //TODO
+  @Input() loading: boolean = false; //TODO
+  @Input() fullWidth: boolean = false; //TODO
+  @Input() shape: "circle" | "rectangle" = "rectangle";
+  @Input() bold: "bold" | "semibold" | "medium" | "normal" | "thin" | "extralight" | "light" = "normal";
   @Input() onClick: () => void = () => { console.log("Button clicked!"); };
 }
--- quizine/src/app/components/landing/landing.component.ts
+++ quizine/src/app/components/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { WebsiteStatusButtonComponent } from '../website-status-button/website-status-button.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'landing',
   standalone: true,
-  imports: [WebsiteStatusButtonComponent],
+  imports: [WebsiteStatusButtonComponent, ButtonComponent],
   templateUrl: './landing.component.html'
 })
 export class LandingComponent {
